e4e51e982a1a154bda7d4f774cbac9300efa7ee6
HabibAroua <habibha.aroua82@gmail.com>
2021-03-31 00:51:11 +0100

    before change the password

--- Admin/template/updatePassword.php
+++ Admin/template/updatePassword.php
@@ -4,47 +4,21 @@
             <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Update your password</h4>
-                        <form class="forms-sample">
+                        <h4 class="card-title">Change your password</h4>
+                        <form class="forms-sample" method="POST" action="../app/action/Admin/updatePassword.php">
                             <div class="form-group">
-                                <label for="exampleInputName1">Name</label>
-                                <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                                <label for="old_password">Old Password</label>
+                                <input type="password" class="form-control" id="old_password" name="old_password" placeholder="Old password">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail3">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+                                <label for="new_password">New password</label>
+                                <input type="password" id="new_password" class="form-control" name="new_password"  placeholder="New password">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword4">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+                                <label for="conf_password">Confirm your new password</label>
+                                <input type="password" id="conf_password" class="form-control"  placeholder="Confirm your new password">
                             </div>
-                            <div class="form-group">
-                                <label for="exampleSelectGender">Gender</label>
-                                <select class="form-control" id="exampleSelectGender">
-                                    <option>Male</option>
-                                    <option>Female</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>File upload</label>
-                                <input type="file" name="img[]" class="file-upload-default">
-                                <div class="input-group col-xs-12">
-                                    <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-                                    <span class="input-group-append">
-                                        <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputCity1">City</label>
-                                <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleTextarea1">Textarea</label>
-                                <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                            <button class="btn btn-light">Cancel</button>
+                            <button id="btUpdatePassword" type="submit" class="btn btn-primary mr-2">Submit</button>
                         </form>
                     </div>
                 </div>
@@ -52,3 +26,53 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready
+	(
+		function()
+		{
+			$("#btUpdatePassword").click
+			(
+				function(e)
+				{
+                    var old_password = $('#old_password').val();
+                    var new_password = $('#new_password').val();
+                    var conf_password = $('#conf_password').val();
+                    if(old_password === "")
+                    {
+                        $("#old_password").focus();
+                        alertify.error('You Should enter your old password');
+                        e.preventDefault();
+                    }
+                    else
+                    {
+                        if(new_password === "")
+                        {
+                            $("#new_password").focus();
+                            alertify.error('You Should enter your new password');
+                            e.preventDefault();
+                        }
+                        else
+                        {
+                            if(conf_password === "")
+                            {
+                                $("#conf_password").focus();
+                                alertify.error('You Should confirm your new password');
+                                e.preventDefault();
+                            }
+                            else
+                            {
+                                if(new_password != conf_password)
+                                {
+                                    $("#conf_password").focus();
+                                    alertify.error('You Should verif your new pasword');
+                                    e.preventDefault();
+                                }
+                            }
+                        }
+                    }
+                }
+            );
+        }
+    );
+</script>
--- app/action/Admin/updatePassword.php
+++ app/action/Admin/updatePassword.php
@@ -1,11 +1,19 @@
 <?php
     session_start ();
-    if($_POST['new_password'])
+    if(($_POST['old_password']) && ($_POST['new_password']))
     {
-        
+        if(sha1($_SESSION['password']) == sha1($_POST['old_password']))
+        {
+            echo "good";
+            //header ('location: ../../../Admin/logout.php');
+        }
+        else
+        {
+            echo "Verif your old password";
+        }
     }
     else
     {
-        echo "Error";
+        
     }
 ?>
