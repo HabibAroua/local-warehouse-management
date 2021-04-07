2d65bc2a86ae09993e278a53e00eaba7d9ffa85a
HabibAroua <habibha.aroua82@gmail.com>
2021-04-07 12:32:58 +0100

    finish the crud op of Provider

--- app/action/Provider/getAll.php
+++ app/action/Provider/getAll.php
@@ -1,4 +1,8 @@
 <?php
     require_once('../../connection/Connection.php');
     require_once('../../model/Admin.php');
+    
+    $provider = new Provider();
+    
+    echo json_encode($provider->getAll());
 ?>
