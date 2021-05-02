4e35495a803ee745cc35211373f9b86244cc6a5d
HabibAroua <habibha.aroua82@gmail.com>
2021-05-02 01:43:14 +0100

    create interface

--- index.php
+++ index.php
@@ -1,52 +1,141 @@
+<!DOCTYPE html>
+<html lang="en" >
 
-<!doctype html>
-<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://unpkg.com/tailwindcss@1.2.0/dist/tailwind.min.css" rel="stylesheet">
+
+  <meta charset="UTF-8">
+  
+<link rel="apple-touch-icon" type="image/png" href="https://cpwebassets.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png" />
+<meta name="apple-mobile-web-app-title" content="CodePen">
+
+<link rel="shortcut icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico" />
+
+<link rel="mask-icon" type="" href="https://cpwebassets.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg" color="#111" />
+
+
+  <title>CodePen - Bootstrap 4 Navbar</title>
+  
+  
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'>
+  
+<style>
+.navbar { background-color: #484848; }
+.navbar .navbar-nav .nav-link { color: #fff; }
+.navbar .navbar-nav .nav-link:hover { color: #fbc531; }
+.navbar .navbar-nav .active > .nav-link { color: #fbc531; }
+
+footer a.text-light:hover { color: #fed136!important; text-decoration: none; }
+footer .cizgi { border-right: 1px solid #535e67; }
+@media (max-width: 992px) { footer .cizgi { border-right: none; } }
+</style>
+
+  <script>
+  window.console = window.console || function(t) {};
+</script>
+
+  
+  
+  <script>
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+</script>
+
+
 </head>
-<body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind Login Template</title>
-    <meta name="author" content="David Grzyb">
-    <meta name="description" content="">
 
-    <!-- Tailwind -->
-    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+<body translate="no" >
+  <header>
+	<!--- Navbar --->
+	<nav class="navbar navbar-expand-lg">
+		<div class="container">
+			<a class="navbar-brand text-white" href="#"><i class="fa fa-graduation-cap fa-lg mr-2"></i>Client</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nvbCollapse" aria-controls="nvbCollapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="nvbCollapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-home fa-fw mr-1"></i>Home</a>
+					</li>
+					<li class="nav-item active pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-th-list fa-fw mr-1"></i>Blog</a>
+					</li>
+					<li class="nav-item pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-info-circle fa-fw mr-1"></i>Information</a>
+					</li>
+					<li class="nav-item pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-phone fa-fw fa-rotate-180 mr-1"></i>İletişim</a>
+					</li>
+					<li class="nav-item pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-user-plus fa-fw mr-1"></i>Kayıt Ol</a>
+					</li>
+					<li class="nav-item pl-1">
+						<a class="nav-link" href="#"><i class="fa fa-sign-in fa-fw mr-1"></i>Oturum Aç</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!--# Navbar #-->
+	</header>
 
-        .font-family-karla {
-            font-family: karla;
-        }
-    </style>
-</head>
-<body class="bg-white font-family-karla h-screen">
-
-    <div class="w-full flex flex-wrap">
-
-        <!-- Login Section -->
-        <div class="w-1/2 shadow-2xl">
-            <a href="Admin/">
-				<img class="object-cover w-full h-screen hidden md:block transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold" src="admin.jpg">
-			</a>
-        </div>
-
-        <!-- Image Section -->
-        <div class="w-1/2 shadow-2xl">
-            <a href="Client/">
-				<img class="object-cover w-full h-screen hidden md:block transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold" src="Client.png">
-			</a>
-        </div>
-    </div>
+<!--- Footer --->
+	<footer>
+		<div class="jumbotron jumbotron-fluid bg-secondary p-4 mt-5 mb-0">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-light text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">Telif Hakkı</h5>
+							<p class="d-inline lead">Tüm Hakları Saklıdır © 2018<br>
+							<a href="#" class="text-light d-block lead">Blog</a>
+							</p>
+	
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">İletişim</h5>
+								<a class="text-light d-block lead" style="margin-left: -20px" href="#"><i class="fa fa-phone mr-2"></i>+90 (000) 000 0 000</a>
+								<a class="text-light d-block lead" href="#"><i class="fa fa-envelope mr-2"></i><span class="__cf_email__" data-cfemail="e9888d848087a985868a888581869a9dc78a8684">[email&#160;protected]</span></a>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+							<h5 class="card-title text-white display-4" style="font-size:30px">Sosyal Medya</h5>
+					
+									<a class="text-light" href="#"><i class="fa fa-facebook-square fa-fw fa-2x"></i></a>
+								
+									<a class="text-light" href="#"><i class="fa fa-twitter-square fa-fw fa-2x"></i></a>
+								
+									<a class="text-light" href="#"><i class="fa fa-instagram fa-fw fa-2x"></i></a>
+								
+									<a class="text-light" href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--# Footer #-->
+  
+  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0-next.1/esm/popper.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'></script>
+  
+  
 
 </body>
-</html>
-</body>
+
 </html>
