b9d7b51f70de69506f07e19ec0cbed4a0a0919cd
HabibAroua <habibha.aroua82@gmail.com>
2021-05-02 01:06:59 +0100

    correct the code

--- app/action/Category/update.php
+++ app/action/Category/update.php
@@ -3,16 +3,15 @@
     require_once ('../../connection/Connection.php');
     require_once ('../../model/Category.php');
     
-    if((isset($_POST['label'])) && (isset($_POST['description'])&&( isset($_POST['id']))))
+    if((isset($_POST['label'])) && (isset($_POST['description'])) && (isset($_POST['id'])))
     {
         $c=new Category ();
-        $c->setLabel($_POST['label']);
+        $c->setLabel(ucfirst($_POST['label']));
         $c->setDescription($_POST['description']);
         
         if ($c->update($_POST['id'])==1)
         {
             echo "Uppdating of category successfully has been completed";
-        
         }
         else
         {
