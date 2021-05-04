19fc8dd834b198792745ac42e723e228891a359d
HabibAroua <habibha.aroua82@gmail.com>
2021-05-04 23:56:57 +0100

    finish the navbar

--- index.php
+++ index.php
@@ -8,11 +8,39 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-md navbar-dar bg-dark">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 			<a class="navbar-brand" href="#">E-Commerce</a>
 			<button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarSupportedContent">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-		</nav>5:15
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mx-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">
+							Home
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							About
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							Services
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							Contact
+						</a>
+					</li>
+				</ul>
+				<form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" class="btn btn-in-light my-sm-0" type="submit" />
+					<button class="btn btn-light my-sm-0">Search</button>
+				</form>
+			</div>
+		</nav>
 	</body>
 </html>
