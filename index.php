e998c1cf50cf8968847fba89bfd20ae3bad343e4
HabibAroua <habibha.aroua82@gmail.com>
2021-05-04 19:00:27 +0100

    update the gui of index page

--- index.php
+++ index.php
@@ -1,141 +1,18 @@
-<!DOCTYPE html>
-<html lang="en" >
-
-<head>
-
-  <meta charset="UTF-8">
-  
-<link rel="apple-touch-icon" type="image/png" href="https://cpwebassets.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png" />
-<meta name="apple-mobile-web-app-title" content="CodePen">
-
-<link rel="shortcut icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico" />
-
-<link rel="mask-icon" type="" href="https://cpwebassets.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg" color="#111" />
-
-
-  <title>CodePen - Bootstrap 4 Navbar</title>
-  
-  
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css'>
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'>
-  
-<style>
-.navbar { background-color: #484848; }
-.navbar .navbar-nav .nav-link { color: #fff; }
-.navbar .navbar-nav .nav-link:hover { color: #fbc531; }
-.navbar .navbar-nav .active > .nav-link { color: #fbc531; }
-
-footer a.text-light:hover { color: #fed136!important; text-decoration: none; }
-footer .cizgi { border-right: 1px solid #535e67; }
-@media (max-width: 992px) { footer .cizgi { border-right: none; } }
-</style>
-
-  <script>
-  window.console = window.console || function(t) {};
-</script>
-
-  
-  
-  <script>
-  if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage("resize", "*");
-  }
-</script>
-
-
-</head>
-
-<body translate="no" >
-  <header>
-	<!--- Navbar --->
-	<nav class="navbar navbar-expand-lg">
-		<div class="container">
-			<a class="navbar-brand text-white" href="#"><i class="fa fa-graduation-cap fa-lg mr-2"></i>Client</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nvbCollapse" aria-controls="nvbCollapse">
+<!doctype html>
+<html>
+	<head>
+		<title>E-Commerce</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dar bg-dark">
+			<a class="navbar-brand" href="#">E-Commerce</a>
+			<button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarSupportedContent">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="nvbCollapse">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-home fa-fw mr-1"></i>Home</a>
-					</li>
-					<li class="nav-item active pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-th-list fa-fw mr-1"></i>Blog</a>
-					</li>
-					<li class="nav-item pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-info-circle fa-fw mr-1"></i>Information</a>
-					</li>
-					<li class="nav-item pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-phone fa-fw fa-rotate-180 mr-1"></i>İletişim</a>
-					</li>
-					<li class="nav-item pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-user-plus fa-fw mr-1"></i>Kayıt Ol</a>
-					</li>
-					<li class="nav-item pl-1">
-						<a class="nav-link" href="#"><i class="fa fa-sign-in fa-fw mr-1"></i>Oturum Aç</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<!--# Navbar #-->
-	</header>
-
-<!--- Footer --->
-	<footer>
-		<div class="jumbotron jumbotron-fluid bg-secondary p-4 mt-5 mb-0">
-			<div class="container">
-				<div class="row">
-					<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 cizgi">
-						<div class="card bg-secondary border-0">
-							<div class="card-body text-light text-center">
-								<h5 class="card-title text-white display-4" style="font-size:30px">Telif Hakkı</h5>
-							<p class="d-inline lead">Tüm Hakları Saklıdır © 2021<br>
-							<a href="#" class="text-light d-block lead">Blog</a>
-							</p>
-	
-							</div>
-						</div>
-					</div>
-					
-					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cizgi">
-						<div class="card bg-secondary border-0">
-							<div class="card-body text-center">
-								<h5 class="card-title text-white display-4" style="font-size:30px">İletişim</h5>
-								<a class="text-light d-block lead" style="margin-left: -20px" href="#"><i class="fa fa-phone mr-2"></i>+90 (000) 000 0 000</a>
-								<a class="text-light d-block lead" href="#"><i class="fa fa-envelope mr-2"></i><span class="__cf_email__" data-cfemail="e9888d848087a985868a888581869a9dc78a8684">[email&#160;protected]</span></a>
-							</div>
-						</div>
-					</div>
-					
-					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-						<div class="card bg-secondary border-0">
-							<div class="card-body text-center">
-							<h5 class="card-title text-white display-4" style="font-size:30px">Social Media</h5>
-					
-									<a class="text-light" href="#"><i class="fa fa-facebook-square fa-fw fa-2x"></i></a>
-								
-									<a class="text-light" href="#"><i class="fa fa-twitter-square fa-fw fa-2x"></i></a>
-								
-									<a class="text-light" href="#"><i class="fa fa-instagram fa-fw fa-2x"></i></a>
-								
-									<a class="text-light" href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
-							</div>
-						</div>
-					</div>	
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!--# Footer #-->
-  
-  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0-next.1/esm/popper.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'></script>
-  
-  
-
-</body>
-
+		</nav>5:15
+	</body>
 </html>
