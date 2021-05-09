731c282683f9f74f81d3c10f55786e09bf493fdc
HabibAroua <habibha.aroua82@gmail.com>
2021-05-09 02:42:38 +0100

    verif who the user is connecting

--- Client/login.php
+++ Client/login.php
@@ -3,7 +3,7 @@
     <?php
         require_once('template/head.php');
     ?>
-    <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">>
+    <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
         <?php   
             require_once('template/client/login.php');
         ?>
--- Client/template/client/login.php
+++ Client/template/client/login.php
@@ -1,6 +1,6 @@
 <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
     <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-        <form>
+        <form method="POST" action="../app/action/Client/auth.php">
             <h1 class="font-bold text-center text-2xl mb-5">Authentification</h1>  
             <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
                 <div class="px-5 py-7">
@@ -8,7 +8,7 @@
                     <input  type="text" id="login" name="login" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
                     <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
                     <input type="password" id="password" name="password" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-                    <button type="submit" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+                    <button type="submit" id="btLogin" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                         <span class="inline-block mr-2">Login</span>
                     </button>
                 </div>
