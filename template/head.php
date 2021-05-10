8f2f16a6a7663b3cb3decb19b962fa5d65e3ceaa
HabibAroua <habibha.aroua82@gmail.com>
2021-05-10 04:37:48 +0100

    optimzation of source code

---
+++ template/head.php
@@ -0,0 +1,12 @@
+<head>
+	<title>E-Commerce</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="template/css/style.css" />
+</head>
---
+++ template/navbar.php
@@ -0,0 +1,78 @@
+<?php
+    function activePage($page)
+    {
+        $Home = "";
+        $About = "";
+        $Services = "";
+        $Contact = "";
+        $Map;
+		
+        switch($page)
+        {
+            case 'Home' : $Home = 'active';
+			break;
+			case 'About' : $About = 'active';
+			break;
+			case 'Services' : $Services = "active";
+			break;
+			case 'Contact' : $Contact = "active";
+			break;
+			case 'Map' : $Map = "active";
+			break;
+			default : $Home = 'active';
+			break;
+        }
+		echo
+		"
+			<nav class='navbar navbar-expand-md navbar-dark bg-dark'>
+				<a class='navbar-brand' href='#'>E-Commerce</a>
+				<button class='navbar-toggler' type='button'data-toggle='collapse' data-target='#navbarSupportedContent'>
+					<span class='navbar-toggler-icon'></span>
+				</button>
+				<div class='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul class='navbar-nav mx-auto'>
+						<li class='nav-item $Home'>
+							<a class='nav-link' href='#'>
+								Home
+							</a>
+						</li>
+						<li class='nav-item $About'>
+							<a class='nav-link' href='#'>
+								About
+							</a>
+						</li>
+						<li class='nav-item $Services'>
+							<a class='nav-link' href='#'>
+								Services
+							</a>
+						</li>
+						<li class='nav-item $Contact'>
+							<a class='nav-link' href='#'>
+								Contact
+							</a>
+						</li>
+						<li class='nav-item $Map'>
+							<a class='nav-link' href='map.php'>
+								Map
+							</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='Admin/'>
+								Admin area
+							</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='Client/'>
+								Client area
+							</a>
+						</li>
+					</ul>
+					<form class='form-inline'>
+						<input class='form-control mr-sm-2' type='search' placeholder='Search' class='btn btn-in-light my-sm-0' type='submit' />
+						<button class='btn btn-light my-sm-0'>Search</button>
+					</form>
+				</div>
+			</nav>
+		";
+    }
+?>
