d18bd04829c3f70d49e87626bfdb1d3a6c3fe8f7
HabibAroua <habibha.aroua82@gmail.com>
2021-05-12 06:31:00 +0100

    integrate the nav sub

--- Public/template/navbar.php
+++ Public/template/navbar.php
@@ -39,6 +39,19 @@
 								About
 							</a>
 						</li>
+						<li class='nav-item dropdown'>
+          <a class='nav-link dropdown-toggle' href='http://example.com' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+        Dropdown link
+        </a>
+        <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
+          <a class='dropdown-item' href='#'>Action</a>
+          <a class='dropdown-item' href='#'>Another action</a>
+          <a class='dropdown-item' href='#'>Something else here</a>
+        </div>
+        </li>
+
+
+
 						<li class='nav-item $Services'>
 							<a class='nav-link' href='services.php'>
 								Services
@@ -69,3 +82,5 @@
 		";
     }
 ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
