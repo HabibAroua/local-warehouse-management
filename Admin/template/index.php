17f9c435ebecc391f7110974b0272af95df3d57e
HabibAroua <habibha.aroua82@gmail.com>
2021-06-03 21:20:51 +0100

    edit the code

--- Admin/template/index.php
+++ Admin/template/index.php
@@ -3,7 +3,6 @@
     require_once('../app/action/Category/nbCategory.php');
     require_once('../app/action/Product/nbProduct.php');
     require_once('../app/action/Client/nbClient.php');
-    
 ?>
 
 <div class="content-wrapper">
@@ -27,7 +26,6 @@
                         <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?php echo $nbClient ?></h3>
                         <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>  
-                    <p class="mb-0 mt-2 text-danger">0.12% <span class="text-black ml-1"><small>(30 days)</small></span></p>
                 </div>
             </div>
         </div>
@@ -39,7 +37,6 @@
                         <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?php echo $nb ; ?></h3>
                         <i class="ti-user icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>  
-                    <p class="mb-0 mt-2 text-danger">0.47% <span class="text-black ml-1"><small>(30 days)</small></span></p>
                 </div>
             </div>
         </div>
@@ -51,7 +48,6 @@
                         <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?php echo $nbcat ; ?></h3>
                         <i class="ti-agenda icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>  
-                    <p class="mb-0 mt-2 text-success">64.00%<span class="text-black ml-1"><small>(30 days)</small></span></p>
                 </div>
             </div>
         </div>
@@ -63,7 +59,6 @@
                         <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?php echo $nbprod; ?></h3>
                         <i class="ti-layers-alt icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>  
-                    <p class="mb-0 mt-2 text-success">23.00%<span class="text-black ml-1"><small>(30 days)</small></span></p>
                 </div>
             </div>
         </div>
